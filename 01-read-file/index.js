3ce74365cdfeab6c91350f3d332e91c485464028
anatonokao <anato.devs@gmail.com>
2024-01-19 19:47:56 +0300

    feat: finish task 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(
+  path.join(__dirname, 'text.txt'),
+  { encoding: 'utf-8' },
+  (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  },
+);
